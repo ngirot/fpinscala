c58651e7baf05280e575c57311cefe027a03d28f
Nicolas Girot <ngirot@hotmail.com>
2015-09-01 22:43:21 +0200

    Exercise 2.4

--- src/main/scala/exercices/gettingstarted/Curry.scala
+++ src/main/scala/exercices/gettingstarted/Curry.scala
@@ -8,4 +8,9 @@ object Curry {
   def curry[A, B, C](f: (A, B) => C): A => (B => C) =
     (a: A) => (b: B) => f(a, b)
 
+  /**
+   * Exercice 2.4
+   */
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C =
+    (a: A, b: B) => f(a)(b)
 }
