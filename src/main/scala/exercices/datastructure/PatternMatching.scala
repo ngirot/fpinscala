385c26abee507afcca08044f8e723acf5be60b12
Nicolas Girot <ngirot@hotmail.com>
2015-09-02 22:45:11 +0200

    Exercise 3.1

---
+++ src/main/scala/exercices/datastructure/PatternMatching.scala
@@ -0,0 +1,11 @@
+package exercices.datastructure
+
+object PatternMatching {
+  
+  /**
+   * Exercice 3.1
+   */
+ def main(args: Array[String]) =
+   // Should display 3
+   print(List.valeurPatternMatching)
+}
