7ab2be08919ed8ba0573fed5a468861be9db6f8c
Nicolas Girot <ngirot@hotmail.com>
2015-09-20 15:14:38 +0200

    Rework exercise 5.2

--- src/main/scala/exercises/strictnesslaziness/Stream.scala
+++ src/main/scala/exercises/strictnesslaziness/Stream.scala
@@ -1,6 +1,7 @@
 package exercises.strictnesslaziness
 
 import scala.annotation.tailrec
+import scala.util.control.TailCalls.TailRec
 
 sealed trait Stream[+A] {
 
@@ -19,9 +20,18 @@ sealed trait Stream[+A] {
   def take(n: Int): Stream[A] =
     this match {
       case _ if n < 0           => throw new IllegalArgumentException
-      case Cons(h, _) if n == 1 => Cons(h, () => Empty: Stream[A])
-      case Cons(h, t) if n > 1  => Cons(h, () => t().take(n - 1))
-      case _                    => Empty
+      case Cons(h, _) if n == 1 => Stream.cons(h(), Stream.empty)
+      case Cons(h, t) if n > 1  => Stream.cons(h(), t().take(n - 1))
+      case _                    => Stream.empty
+    }
+
+  @tailrec
+  final def drop(n: Int): Stream[A] =
+    this match {
+      case _ if n < 0  => throw new IllegalArgumentException
+      case Empty       => Empty
+      case _ if n == 0 => this
+      case Cons(_, t)  => t().drop(n - 1)
     }
 
   /**
@@ -31,7 +41,7 @@ sealed trait Stream[+A] {
     this match {
       case Empty                  => Empty
       case Cons(h, t) if (f(h())) => Stream.cons(h(), t().takeWhile(f))
-      case Cons(h, t)             => Stream.empty
+      case _                      => Stream.empty
     }
 }
 
--- src/test/scala/exercises/strictnesslaziness/StreamTest.scala
+++ src/test/scala/exercises/strictnesslaziness/StreamTest.scala
@@ -30,15 +30,33 @@ class StreamTest extends FunSuite {
     assert(Stream(1, 2).take(3).toList == Stream(1, 2).toList)
   }
 
-  test("TakeWhild should return all the first element matching a function") {
+  test("Drop should return a Stream without its nth elements") {
+    assert(Stream(1, 2, 3, 4, 5).drop(3).toList == Stream(4, 5).toList)
+  }
+
+  test("Drop should return an empty Steam when remove all elements") {
+    assert(Stream(1, 2).drop(2).toList == Nil)
+  }
+
+  test("Drop should throw an error when n is negative") {
+    intercept[IllegalArgumentException] {
+      Stream(1, 2).drop(-1)
+    }
+  }
+
+  test("Drop should return the entire stream when remove no elements") {
+    assert(Stream(1, 2).drop(0).toList == Stream(1, 2).toList)
+  }
+
+  test("TakeWhile should return all the first element matching a function") {
     assert(Stream(2, 4, 6, 5, 8).takeWhile(_ % 2 == 0).toList == Stream(2, 4, 6).toList)
   }
 
-  test("TakeWhild should return an empty list if the very first element does not match the function") {
+  test("TakeWhile should return an empty list if the very first element does not match the function") {
     assert(Stream(1, 4).takeWhile(_ % 2 == 0).toList == Stream.empty.toList)
   }
 
-  test("TakeWhild should return the entire list if all elements matches the function") {
+  test("TakeWhile should return the entire list if all elements matches the function") {
     assert(Stream(2, 4).takeWhile(_ % 2 == 0).toList == Stream(2, 4).toList)
   }
 }
